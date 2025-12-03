9c4dea12d7a83cb648c6ea243249614ce4c2fdb8
grantimaharafoundation <michael@grantimaharafoundation.org>
2025-12-03 15:55:42 -0800

    Remove autostart

--- lib/pbio/sys/main.c
+++ lib/pbio/sys/main.c
@@ -92,18 +92,6 @@ int main(int argc, char **argv) {
 
     // Automatically start program on boot
 
-    // Ensure the Bluetooth driver is fully ready before requesting the program start.
-    // Otherwise, the program will run briefly and then stop
-    // pbsys_init() starts Bluetooth initialization, but might not wait for it to be complete.
-    while (!pbdrv_bluetooth_is_ready()) {
-        pbio_do_one_event();
-    }
-
-
-
-    pbsys_main_program_request_start(PBIO_PYBRICKS_USER_PROGRAM_ID_FIRST_SLOT, PBSYS_MAIN_PROGRAM_START_REQUEST_TYPE_BOOT);
-
-        pbio_do_one_event();
 
     // Keep loading and running user programs until shutdown is requested.
     while (!pbsys_status_test(PBIO_PYBRICKS_STATUS_SHUTDOWN_REQUEST)) {
