5ba5d1d854d9904caf2229a03497ab898bc39740
grantimaharafoundation <michael@grantimaharafoundation.org>
2025-12-02 21:03:12 -0800

    Noth attempt

--- lib/pbio/sys/main.c
+++ lib/pbio/sys/main.c
@@ -91,18 +91,18 @@ int main(int argc, char **argv) {
     // Ensure the Bluetooth driver is fully ready before requesting the program start.
     // Otherwise, the program will run briefly and then stop
     // pbsys_init() starts Bluetooth initialization, but might not wait for it to be complete.
-    while (!pbdrv_bluetooth_is_ready()) {
+    /*while (!pbdrv_bluetooth_is_ready()) {
         pbio_do_one_event();
     }
-    pbsys_main_program_request_start(0, PBSYS_MAIN_PROGRAM_START_REQUEST_TYPE_HUB_UI);
+    pbsys_main_program_request_start(PBIO_PYBRICKS_USER_PROGRAM_ID_FIRST_SLOT, PBSYS_MAIN_PROGRAM_START_REQUEST_TYPE_BOOT);*/
     //#endif
 
     // Keep loading and running user programs until shutdown is requested.
     while (!pbsys_status_test(PBIO_PYBRICKS_STATUS_SHUTDOWN_REQUEST)) {
 
-        #if PBSYS_CONFIG_USER_PROGRAM_AUTO_START
+        //#if PBSYS_CONFIG_USER_PROGRAM_AUTO_START
         pbsys_main_program_request_start(PBIO_PYBRICKS_USER_PROGRAM_ID_REPL, PBSYS_MAIN_PROGRAM_START_REQUEST_TYPE_BOOT);
-        #endif
+        //#endif
 
         // REVISIT: this can be long waiting, so we could do a more efficient
         // wait (i.e. __WFI() on embedded system)
