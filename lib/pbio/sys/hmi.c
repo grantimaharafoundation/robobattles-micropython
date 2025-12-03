41ff047509234cd7b7aeae4f7ea570041c8ed82d
grantimaharafoundation <michael@grantimaharafoundation.org>
2025-12-02 19:51:37 -0800

    Waste

--- lib/pbio/sys/hmi.c
+++ lib/pbio/sys/hmi.c
@@ -67,11 +67,11 @@ static PT_THREAD(update_program_run_button_wait_state(bool button_pressed)) {
         PT_WAIT_UNTIL(pt, button_pressed);
         PT_WAIT_UNTIL(pt, !button_pressed);
 
-        /*#if PBSYS_CONFIG_BLUETOOTH_TOGGLE
+        #if PBSYS_CONFIG_BLUETOOTH_TOGGLE
         // If we made it through a full press and release, without the user
         // program running, then start the currently selected user program.
         pbsys_main_program_request_start(selected_slot, PBSYS_MAIN_PROGRAM_START_REQUEST_TYPE_HUB_UI);
-        #else*/
+        #else
         if (pbsys_status_test(PBIO_PYBRICKS_STATUS_USER_PROGRAM_RUNNING)) {
             // Short press shuts down hub when program is running
             pbsys_status_set(PBIO_PYBRICKS_STATUS_SHUTDOWN_REQUEST);
@@ -79,7 +79,7 @@ static PT_THREAD(update_program_run_button_wait_state(bool button_pressed)) {
             // Short press tries to start running program when in bluetooth mode
             pbsys_main_program_request_start(selected_slot, PBSYS_MAIN_PROGRAM_START_REQUEST_TYPE_HUB_UI);
         }
-        //#endif
+        #endif
     }
 
     PT_END(pt);
