6341948e138745a7469c1d0669b0b6bec7ce991f
grantimaharafoundation <michael@grantimaharafoundation.org>
2025-12-03 15:28:07 -0800

    IDea

--- lib/pbio/sys/bluetooth.c
+++ lib/pbio/sys/bluetooth.c
@@ -310,9 +310,11 @@ PROCESS_THREAD(pbsys_bluetooth_process, ev, data) {
         pbdrv_bluetooth_power_on(true);
         PROCESS_WAIT_UNTIL(pbdrv_bluetooth_is_ready());
 
-        // Start advertising, and show visual indicator on status light.
-        pbdrv_bluetooth_start_advertising();
-        pbsys_status_set(PBIO_PYBRICKS_STATUS_BLE_ADVERTISING);
+        if (!pbsys_status_test(PBIO_PYBRICKS_STATUS_USER_PROGRAM_RUNNING)) {
+            // Start advertising, and show visual indicator on status light.
+            pbdrv_bluetooth_start_advertising();
+            pbsys_status_set(PBIO_PYBRICKS_STATUS_BLE_ADVERTISING);
+        }
 
         // Now we are idle. We need to change the Bluetooth state and
         // indicators if a host connects to us, or a user program starts, or we
