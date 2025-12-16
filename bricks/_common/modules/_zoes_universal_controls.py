8dfb96813e9598ba3b82f1ae39af97bd34ca619a
grantimaharafoundation <michael@grantimaharafoundation.org>
2025-12-15 19:46:31 -0800

    Formatting fix

--- bricks/_common/modules/_zoes_universal_controls.py
+++ bricks/_common/modules/_zoes_universal_controls.py
@@ -10,11 +10,11 @@ weapon = Motor(Port.C)
 def apply_deadzone(value):
     # Set joystick deadzone
     deadzone = 20
-    
+
     # Input falls within deadzone, so ignore
     if abs(value) < deadzone:
         return 0
-    
+
     # Scale so that deadzone → 0 and max → 1
     sign = 1 if value > 0 else -1
     scaled = (abs(value) - deadzone) / (1 - deadzone / 100)
@@ -30,10 +30,10 @@ while True:
     
     # Reduce left/right input for slower turning to make bot easier to control
     compX /= 3
-    
+
     # Control weapon motors
     weapon.go(compWL - compWR) 
-    
+
     # Control wheel motors
     if compX != 0 or compY != 0:
         # Joystick is pressed, so move wheel motors
@@ -43,3 +43,4 @@ while True:
         # Joystick is not pressed, so let wheel motors spin freely to save power
         left.stop()
         right.stop()
+    
