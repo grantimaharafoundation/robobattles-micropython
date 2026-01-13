cfda1e4f2062d477d52b397e6db4fd5e71b9d761
grantimaharafoundation <michael@grantimaharafoundation.org>
2026-01-12 16:06:49 -0800

    Fix formatting

--- pybricks/iodevices/pb_type_iodevices_xbox_controller.c
+++ pybricks/iodevices/pb_type_iodevices_xbox_controller.c
@@ -410,10 +410,10 @@ static mp_obj_t pb_type_xbox_make_new(const mp_obj_type_t *type, size_t n_args, 
                 "Is the controller in pairing mode?"
                 ));
 
-                // If this is reached, the controller's bonds with both this hub and the other hub will be been broken.
-                // Clear stored address so this hub can at least indicate no pairing. No apparent way to detect and indicate on the other hub.
-                uint8_t zero_addr[6] = {0};
-                pbsys_storage_set_user_data(0, zero_addr, 6);*/
+            // If this is reached, the controller's bonds with both this hub and the other hub will be been broken.
+            // Clear stored address so this hub can at least indicate no pairing. No apparent way to detect and indicate on the other hub.
+            uint8_t zero_addr[6] = {0};
+            pbsys_storage_set_user_data(0, zero_addr, 6);*/
         }
         nlr_jump(nlr.ret_val);
     }
