197271be350cbe56c791ac7eef39a8e60540a5c2
grantimaharafoundation <michael@grantimaharafoundation.org>
2026-01-17 17:26:07 -0800

    Disable light matrix on prime hub

--- bricks/primehub/mpconfigport.h
+++ bricks/primehub/mpconfigport.h
@@ -22,7 +22,7 @@
 #define PYBRICKS_PY_COMMON_KEYPAD               (1)
 #define PYBRICKS_PY_COMMON_KEYPAD_HUB_BUTTONS   (4)
 #define PYBRICKS_PY_COMMON_LIGHT_ARRAY          (1)
-#define PYBRICKS_PY_COMMON_LIGHT_MATRIX         (1)
+#define PYBRICKS_PY_COMMON_LIGHT_MATRIX         (0)
 #define PYBRICKS_PY_COMMON_LOGGER               (1)
 #define PYBRICKS_PY_COMMON_MOTOR_MODEL          (1)
 #define PYBRICKS_PY_COMMON_MOTORS               (1)
